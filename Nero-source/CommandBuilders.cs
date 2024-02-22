43314f2814e556512eae52cc91de47638b27f996
XxWilczUxX <95825590+XxWilczUxX@users.noreply.github.com>
2024-02-22 21:07:21 +0100

    Remake (#4)
    * Made a admin command tree
    
    * Little Udates :3
    
    * Debug Commands Updates
    
    * Debug Commands EmbedBuilder
    
    * Started Character Command Tree
    
    * Debug Command Tree Patches
    
    * Final Debug Command Layout(i think)
    
    * Made code more readable
    
    * Character Command 1
    
    * Roll Command
    
    * Syntax patching

---
+++ Nero-source/CommandBuilders.cs
@@ -0,0 +1,94 @@
+using Discord;
+
+namespace Nero
+{
+
+    public class CommandBuilders
+    {
+
+        public readonly SlashCommandBuilder Debug = new SlashCommandBuilder()
+            .WithName("debug")
+            .WithDescription("debug commands that are only for impactfull management of the bot")
+            .WithDefaultMemberPermissions(GuildPermission.Administrator)
+            .AddOption(new SlashCommandOptionBuilder()
+                .WithName("list")
+                .WithDescription("Gives a list of all commands")
+                .WithType(ApplicationCommandOptionType.SubCommand)
+                .AddOption(new SlashCommandOptionBuilder()
+                    .WithName("type")
+                    .WithDescription("Choose a type")
+                    .WithRequired(true)
+                    .WithType(ApplicationCommandOptionType.Integer)
+                    .AddChoice("guild", 0)
+                    .AddChoice("global", 1)
+                )
+            )
+            .AddOption(new SlashCommandOptionBuilder()
+                .WithName("delete")
+                .WithDescription("Deletes all commands of given type")
+                .WithType(ApplicationCommandOptionType.SubCommand)
+                .AddOption(new SlashCommandOptionBuilder()
+                    .WithName("type")
+                    .WithDescription("Choose a type")
+                    .WithRequired(true)
+                    .WithType(ApplicationCommandOptionType.Integer)
+                    .AddChoice("guild", 0)
+                    .AddChoice("global", 1)
+                )
+            )
+        ;
+
+        public readonly SlashCommandBuilder Character = new SlashCommandBuilder()
+            .WithName("character")
+            .WithDescription("Character edition command tree")
+            .AddOption(new SlashCommandOptionBuilder()
+                .WithName("create")
+                .WithDescription("Starts Character Creation Process")
+                .WithType(ApplicationCommandOptionType.SubCommand)
+                .AddOption(new SlashCommandOptionBuilder()
+                    .WithName("name")
+                    .WithDescription("Provide your character's name")
+                    .WithType(ApplicationCommandOptionType.String)
+                    .WithRequired(true)
+                )
+                .AddOption(new SlashCommandOptionBuilder()
+                    .WithName("nickname")
+                    .WithDescription("Provide your character's nickname")
+                    .WithType(ApplicationCommandOptionType.String)
+                    .WithRequired(true)
+                )
+                .AddOption(new SlashCommandOptionBuilder()
+                    .WithName("class")
+                    .WithDescription("Provide your character's class")
+                    .WithType(ApplicationCommandOptionType.Integer)
+                    .WithRequired(true)
+                    .AddChoice("Solo", 0)
+                    .AddChoice("Netrunner", 1)
+                    .AddChoice("Techie", 2)
+                    .AddChoice("Media", 3)
+                    .AddChoice("Cop", 4)
+                    .AddChoice("Nomad", 5)
+                    .AddChoice("Fixer", 6)
+                    .AddChoice("Corporate", 7)
+                    .AddChoice("Medtech", 8)
+                    .AddChoice("Rockerboy / Rockergirl", 9)
+                )
+                
+            )
+        ;
+        
+        public readonly SlashCommandBuilder Roll = new SlashCommandBuilder()
+            .WithName("roll")
+            .WithDescription("Rolls a dice with given number of sides")
+            .AddOption(new SlashCommandOptionBuilder()
+                .WithName("sides")
+                .WithDescription("type in number of sides")
+                .WithRequired(true)
+                .WithType(ApplicationCommandOptionType.Integer)
+            )
+        ;
+
+
+    }
+
+}
--- Nero-source/Program.cs
+++ Nero-source/Program.cs
@@ -1,22 +1,20 @@
 ﻿using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
-using System;
-using System.Threading.Tasks;
 using Newtonsoft.Json;
-using Discord.Interactions;
 
 namespace Nero
 {
 
     public class Info
     {
-        public string token { get; set; }
-        public ulong basementGuildID { get; set; }
+        public string Token { get; set; }
+        public ulong BasementGuildID { get; set; }
     }
 
     public class Names
     {
+        public readonly string[] stats = { "Inteligence", "Reflex", "Agility", "Technology", "Charisma", "Will", "Luck", "Movement", "Body", "Empathy" };
         public readonly string[] abilityGroups = { "Long Range Weapons", "Body", "Education", "Control", "Observation", "Technology", "Social", "Meele", "Performances" };
         public readonly string[] abilities = { "Heavy Weapons (x2)", "Long Weapons", "Short Weapons", "Archery", "Continous Fire", "Athletics", "Rubber Man", "Torture/Narcotic Tolerancy", "Stealth", "Dance", "Endurance", "Bureaucracy", "Deduction", "Language", "Composing", "Criminology", "Cryptography", "Accounting", "Science", "Animal Care", "Library Searching", "Making Deals", "Art of Survival", "Tactics", "Local Knowlege", "Education", "Riding", "Piloting (x2)", "Car Driving", "Sailing", "Lip Reading", "Concentration", "Perception", "Tracking", "Hiding/Finding an Item", "Cyber Engineering", "Electronics and Security", "Falsification", "Photography", "Pickpocketing", "Art", "Explosives", "Weapon Repair", "Land Vehicle Repair", "Water Vehicle Repair", "Air Vehicle Repair", "Lockpicking", "First Aid", "Basic Repairing", "Paramedics", "Attractiveness", "Trading", "Conversation", "Fashion", "Emotional Inteligence", "Persuasion", "Bribery", "Interrogation", "Semi-Literate Knowlege", "Fighting", "Meele Weapons", "Martial Arts", "Dodgeing", "Acting", "Playing an Instrument" };
         public readonly string[] roles = { "Solo", "Netrunner", "Techie", "Media", "Cop", "Nomad", "Fixer", "Corporate", "Medtech", "Rockerboy / Rockergirl" };
@@ -26,28 +24,25 @@ namespace Nero
     class Program
     {
 
-        static void Main(string[] args)
+        static void Main()
             => new Program().MainAsync().GetAwaiter().GetResult();
 
         private DiscordSocketClient _client;
         private CommandService _commands;
-        Info info = JsonConvert.DeserializeObject<Info>(File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), "Nero-source/json/safe/safe.json" )));
-        Names names = new Names();
+        readonly Info info = JsonConvert.DeserializeObject<Info>(File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), "Nero-source/json/safe/safe.json")));
         public async Task MainAsync()
         {   
+
             _client = new DiscordSocketClient();
             _commands = new CommandService();
 
             _client.Log += Log;
             _client.Ready += Client_Ready;
 
-            string token = info.token;
+            string Token = info.Token;
 
-
-            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.LoginAsync(TokenType.Bot, Token);
             await _client.StartAsync();
-
-            //_client.MessageReceived += MessageReceived; 
 
             await Task.Delay(-1);
         }
@@ -62,99 +57,20 @@ namespace Nero
         public async Task Client_Ready()
         {
 
-            var guild = _client.GetGuild(info.basementGuildID);
+            var guild = _client.GetGuild(info.BasementGuildID);
 
-            /*
-
-            var guildCommand = new SlashCommandBuilder()
-                .WithName("help")
-
-            */
-
-            // Guild commands delete all
-
-            /*
-
-            var commands = await guild.GetApplicationCommandsAsync();
-
-            foreach(var command in commands)
-            {
-                command.DeleteAsync();
-            }
-        
-            */
-
-            // character command tree
-
-            /*
-            
-            var guildCommand = new SlashCommandBuilder()
-                .WithName("character")
-                .WithDescription("character managament command tree")
-                .AddOption(new SlashCommandOptionBuilder()
-                    .WithName("list")
-                    .WithDescription("Lists out all characters")
-                    .WithType(ApplicationCommandOptionType.SubCommand)
-                )
-                .AddOption(new SlashCommandOptionBuilder()
-                    .WithName("create")
-                    .WithDescription("Creates a character")
-                    .AddOption("name", ApplicationCommandOptionType.String, "Name and surname of your character", isRequired: true)
-                    .AddOption("nickname", ApplicationCommandOptionType.String, "The nickname of your character", isRequired: true)
-                    .WithType(ApplicationCommandOptionType.SubCommand)
-                    .AddOption(new SlashCommandOptionBuilder()
-                        .WithName("role")
-                        .WithDescription("Choose character's role")
-                        .WithRequired(true)
-                        .WithType(ApplicationCommandOptionType.Integer)
-                    )
-                    .AddOption("description", ApplicationCommandOptionType.String, "A short description of your character", isRequired: true)
-                    .AddOption("main-stats", ApplicationCommandOptionType.String, "Stats for: INT, REF, AGI, TECH, CHA, SW, LUC, MOV, BC, EMP. ex: \"8,6,7,8,5,6,5,6,3,8\" (sum 62)", isRequired: true)
-                )
-                .AddOption(new SlashCommandOptionBuilder()
-                    .WithName("edit")
-                    .WithDescription("Edits a certain thing about a character")
-                    .WithType(ApplicationCommandOptionType.SubCommand)
-                    
-                );
-
-            for(int i = 0; i < names.roles.Length; i++)
-            {
-                guildCommand.Options[1].Options[2].AddChoice(names.roles[i], i);
-            }
-
-            
+            var guildCommand = new SlashCommandBuilder();
 
             try
             {
-                await _client.Rest.CreateGuildCommand(guildCommand.Build(), info.basementGuildID);
+                await guild.CreateApplicationCommandAsync(guildCommand.Build());
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
+
                 Console.WriteLine(ex.Message);
             }
 
-            */
-
-            // - Global Slash Command Template
-            /* 
-
-            var globalCommand = new SlashCommandBuilder();
-                globalCommand.WithName("name");
-                globalCommand.WithDescription("desc");
-
-            try
-            {
-
-                await _client.CreateGlobalApplicationCommandAsync(globalCommand.Build());
-
-            }
-            catch(Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-
-            */
 
             _client.SlashCommandExecuted += SlashCommandHandler;
 
@@ -162,78 +78,20 @@ namespace Nero
 
         private async Task SlashCommandHandler(SocketSlashCommand command)
         {
-            Console.WriteLine($"Executed: {command.Data.Name}_{command.Data.Id}");
+            Console.WriteLine($"Executed: {command.Data.Name}, ID: {command.Data.Id} \n");
 
-            
-
-            switch(command.Data.Name)
+            switch(command.CommandName)
             {
+                case "debug":
+                    var debug = new DebugCommands();
+                    await debug.CommandHandler(command, _client.GetGuild(info.BasementGuildID), _client);
+                    break;
                 case "roll":
-                    await RollHandler(command);
+                    var comm = new GeneralCommands();
+                    await comm.Roll(command);
                     break;
-                case "character":
-                    var cl = new CharacterEditor();
-                    await cl.CharacterHandler(command, command.Data.Options.First().Name);
-                    break;
-
-                
             }
-
         }
-
-        private async Task RollHandler(SocketSlashCommand command)
-        {
-            var commandOptions = command.Data.Options.Count;
-            var rng = new Random();
-            int[] outputs = new int[1];
-            
-            try
-            {
-                outputs = new int[Convert.ToInt32(command.Data.Options.ElementAt(1).Value)];
-            }
-            catch
-            {
-                outputs = new int[1];
-            }
-            int num = 0;   
-
-            num = rng.Next(20,50);     
-
-            // i: number of repeating all rolling, j: number of dices to roll
-
-            if(commandOptions == 1)
-            {
-                for(int i = 0; i < num; i++)
-                {
-                    outputs[0] = rng.Next(1, Convert.ToInt32(command.Data.Options.ElementAt(0).Value) + 1 );
-                }
-            } 
-            else
-            {
-                for(int i = 0; i < num; i++)
-                {
-                    for(int j = 0; j < outputs.Length; j++)
-                    {
-                        outputs[j] = rng.Next(1, Convert.ToInt32(command.Data.Options.ElementAt(0).Value) + 1 );
-                    }
-                }
-            }
-
-            
-            string title = $"**D{command.Data.Options.ElementAt(0).Value}**";
-            var output = String.Join($"\n", outputs);
-            var sum = outputs.Sum();
-
-            var embedBuilder = new EmbedBuilder()
-                .WithColor(Color.Gold)
-                .WithTitle(title)
-                .WithDescription(output);
-
-            await command.RespondAsync(embed: embedBuilder.Build());
-
-        }
-
-        
 
     }
 }

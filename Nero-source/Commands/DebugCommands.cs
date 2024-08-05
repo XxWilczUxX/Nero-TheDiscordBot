f935b34feb80efe481c775388fa1acb09b36f231
smellilac <dimatega@gmail.com>
2024-08-06 01:07:05 +0300

    Bump Discord.Net to 3.15.3 and make code cleanup

--- Nero-source/Commands/DebugCommands.cs
+++ Nero-source/Commands/DebugCommands.cs
@@ -1,137 +1,124 @@
 using Discord;
 using Discord.WebSocket;
 
-namespace Nero
+namespace Nero;
+public class DebugCommands
 {
-
-    public class DebugCommands
+    public async Task CommandHandler(SocketSlashCommand command, SocketGuild guild, DiscordSocketClient _client, Info info)
     {
-
-        public async Task CommandHandler(SocketSlashCommand command, SocketGuild guild, DiscordSocketClient _client, Info info)
+        switch (command.Data.Options.First().Name)
         {
-            switch(command.Data.Options.First().Name)
-            {
-                case "list":
-                    switch(command.Data.Options.First().Options.First().Value.ToString())
-                    {
-                        case "0":
-                            await ListAllGuildCommands(command, guild);
-                            break;
-                        case "1":
-                            await ListAllGlobalCommands(command, _client);
-                            break;
-                    }
-                    break;
-                case "delete":
-                    switch(command.Data.Options.First().Options.First().Value.ToString())
-                    {
-                        case "0":
-                            await DeleteAllGuildCommands(command, guild);
-                            break;
-                        case "1":
-                            await DeleteAllGlobalCommands(command, _client);
-                            break;
-                    }
-                    break;
-                case "shutdown":
-
-                    if(command.User.Id == info.HeadAdminID) {
-
-                        var embeds = new Embeds();
-                        await command.RespondAsync("",embed: embeds.Info("Shutdown", "The bot has been shut down.").Build())
-                            .ContinueWith(async _ =>{
-                                await _client.LogoutAsync();
-                                Console.WriteLine("Logged out...");
-
-                                await _client.StopAsync();
-                                Console.WriteLine("Client stopped...");
-
-                                await Task.Delay(TimeSpan.FromSeconds(2));
-
-                                Console.WriteLine("Exiting...");
-                                Environment.Exit(0);
-                            });
-
-                    } else {
-                        var embeds = new Embeds();
-                        await command.RespondAsync("",embed: embeds.Error("You don't have permissions to run this command.").Build());
-                    }
-                    break;
-                //
-            }
-        }
-
-        // List commands
-
-        public static async Task ListAllGuildCommands(SocketSlashCommand command, SocketGuild guild)
-        {
-
-            var commands = await guild.GetApplicationCommandsAsync();
-            var embed = new EmbedBuilder()
-                .WithTitle("List of Guild Commands")
-                .WithColor(Color.DarkBlue)
-                .WithCurrentTimestamp();
-            foreach(var com in commands)
-            {
-                embed.AddField(com.Name, $"ID: {com.Id}");
-            }
-            
-            await command.RespondAsync(embed: embed.Build());
-        }
-
-        public static async Task ListAllGlobalCommands(SocketSlashCommand command, DiscordSocketClient _client)
-        {
-
-            var commands = await _client.GetGlobalApplicationCommandsAsync();
-            var embed = new EmbedBuilder()
-                .WithTitle("List of Global Commands")
-                .WithColor(Color.DarkBlue)
-                .WithCurrentTimestamp();
-            foreach(var com in commands)
-            {
-                embed.AddField(com.Name, $"ID: {com.Id}");
-            }
-            
-            await command.RespondAsync(embed: embed.Build());
-        }
-
-        // Delete commands
-
-        public static async Task DeleteAllGuildCommands(SocketSlashCommand command, SocketGuild guild)
-        {
-            var commands = await guild.GetApplicationCommandsAsync();
-
-            foreach(var delCom in commands)
-            {
-                if(delCom.Id != command.Data.Id)
+            case "list":
+                switch(command.Data.Options.First().Options.First().Value.ToString())
                 {
-                    Console.WriteLine($"Deleted {delCom.Name}: {delCom.Id}");
-                    await delCom.DeleteAsync();
+                    case "0":
+                        await ListAllGuildCommands(command, guild);
+                        break;
+                    case "1":
+                        await ListAllGlobalCommands(command, _client);
+                        break;
                 }
-            }
-            Embeds embeds = new Embeds();
-            EmbedBuilder embed = embeds.DebugExecuted();
-            await command.RespondAsync(embed: embed.Build());
-
-        }
-
-        public static async Task DeleteAllGlobalCommands(SocketSlashCommand command, DiscordSocketClient _client)
-        {
-            var commands = await _client.GetGlobalApplicationCommandsAsync();
-            foreach(var delCom in commands)
-            {
-                if(delCom.Id != command.Id)
+                break;
+            case "delete":
+                switch(command.Data.Options.First().Options.First().Value.ToString())
                 {
-                    Console.WriteLine($"Deleted {delCom.Name}: {delCom.Id}");
-                    await delCom.DeleteAsync();
+                    case "0":
+                        await DeleteAllGuildCommands(command, guild);
+                        break;
+                    case "1":
+                        await DeleteAllGlobalCommands(command, _client);
+                        break;
                 }
-            }
-            Embeds embeds = new Embeds();
-            var embed = embeds.DebugExecuted();
-            await command.RespondAsync(embed: embed.Build());
+                break;
+            case "shutdown":
 
+                if(command.User.Id == info.HeadAdminID) {
+
+                    var embeds = new Embeds();
+                    await command.RespondAsync("",embed: embeds.Info("Shutdown", "The bot has been shut down.").Build())
+                        .ContinueWith(async _ =>{
+                            await _client.LogoutAsync();
+                            Console.WriteLine("Logged out...");
+
+                            await _client.StopAsync();
+                            Console.WriteLine("Client stopped...");
+
+                            await Task.Delay(TimeSpan.FromSeconds(2));
+
+                            Console.WriteLine("Exiting...");
+                            Environment.Exit(0);
+                        });
+
+                } else {
+                    var embeds = new Embeds();
+                    await command.RespondAsync("",embed: embeds.Error("You don't have permissions to run this command.").Build());
+                }
+                break;
         }
-
     }
 
+    // List commands
+    public static async Task ListAllGuildCommands(SocketSlashCommand command, SocketGuild guild)
+    {
+        var commands = await guild.GetApplicationCommandsAsync();
+        var embed = new EmbedBuilder()
+            .WithTitle("List of Guild Commands")
+            .WithColor(Color.DarkBlue)
+            .WithCurrentTimestamp();
+        foreach(var com in commands)
+        {
+            embed.AddField(com.Name, $"ID: {com.Id}");
+        }
+        
+        await command.RespondAsync(embed: embed.Build());
+    }
+
+    public static async Task ListAllGlobalCommands(SocketSlashCommand command, DiscordSocketClient _client)
+    {
+        var commands = await _client.GetGlobalApplicationCommandsAsync();
+        var embed = new EmbedBuilder()
+            .WithTitle("List of Global Commands")
+            .WithColor(Color.DarkBlue)
+            .WithCurrentTimestamp();
+        foreach(var com in commands)
+        {
+            embed.AddField(com.Name, $"ID: {com.Id}");
+        }
+        
+        await command.RespondAsync(embed: embed.Build());
+    }
+
+    // Delete commands
+    public static async Task DeleteAllGuildCommands(SocketSlashCommand command, SocketGuild guild)
+    {
+        var commands = await guild.GetApplicationCommandsAsync();
+
+        foreach(var delCom in commands)
+        {
+            if(delCom.Id != command.Data.Id)
+            {
+                Console.WriteLine($"Deleted {delCom.Name}: {delCom.Id}");
+                await delCom.DeleteAsync();
+            }
+        }
+        Embeds embeds = new Embeds();
+        EmbedBuilder embed = embeds.DebugExecuted();
+        await command.RespondAsync(embed: embed.Build());
+    }
+
+    public static async Task DeleteAllGlobalCommands(SocketSlashCommand command, DiscordSocketClient _client)
+    {
+        var commands = await _client.GetGlobalApplicationCommandsAsync();
+        foreach(var delCom in commands)
+        {
+            if(delCom.Id != command.Id)
+            {
+                Console.WriteLine($"Deleted {delCom.Name}: {delCom.Id}");
+                await delCom.DeleteAsync();
+            }
+        }
+        Embeds embeds = new Embeds();
+        var embed = embeds.DebugExecuted();
+        await command.RespondAsync(embed: embed.Build());
+    }
 }
--- Nero-source/Embeds.cs
+++ Nero-source/Embeds.cs
@@ -1,66 +1,58 @@
 using Discord;
-using Discord.WebSocket;
 
-namespace Nero
+namespace Nero;
+
+public class Embeds
 {
-
-    public class Embeds
+    public EmbedBuilder Error(string errorMessage)
     {
-
-        public EmbedBuilder Error(string errorMessage)
-        {
-            var embed = new EmbedBuilder()
-                .WithTitle("Error")
-                .WithDescription(errorMessage)
-                .WithColor(Color.Red)
-                .WithCurrentTimestamp();
-            
-            return embed;
-        }
-
-        public EmbedBuilder Info(string title, string infoMessage)
-        {
-            var embed = new EmbedBuilder()
-                .WithTitle(title)
-                .WithDescription(infoMessage)
-                .WithColor(Color.Green)
-                .WithCurrentTimestamp();
-            
-            return embed;
-        }
-
-        public EmbedBuilder DebugExecuted()
-        {
-            var embed = new EmbedBuilder()
-                .WithTitle("Done")
-                .WithColor(Color.DarkBlue)
-                .WithCurrentTimestamp();
-
-            return embed;
-        }
-
-        public EmbedBuilder Rolls(int dice, int[] rolls)
-        {
-            var embed = new EmbedBuilder()
-                .WithTitle($"Rolled D{dice}, {rolls.Length} times:\n\nSum: {rolls.Sum()} out of {dice*rolls.Length}")
-                .WithDescription($"Breakdown of rolls:")
-                .WithColor(Color.Orange)
-            ;
-
-            for(int i = 0; i < rolls.Length; i++) {
-
-                var field = new EmbedFieldBuilder()
-                    .WithName($"Roll no. {i+1}")
-                    .WithValue(rolls[i])
-                    .WithIsInline(false)
-                ;
-
-                embed.AddField(field);
-            }
-
-            return embed;
-        }
-
+        var embed = new EmbedBuilder()
+            .WithTitle("Error")
+            .WithDescription(errorMessage)
+            .WithColor(Color.Red)
+            .WithCurrentTimestamp();
+        
+        return embed;
     }
 
+    public EmbedBuilder Info(string title, string infoMessage)
+    {
+        var embed = new EmbedBuilder()
+            .WithTitle(title)
+            .WithDescription(infoMessage)
+            .WithColor(Color.Green)
+            .WithCurrentTimestamp();
+        
+        return embed;
+    }
+
+    public EmbedBuilder DebugExecuted()
+    {
+        var embed = new EmbedBuilder()
+            .WithTitle("Done")
+            .WithColor(Color.DarkBlue)
+            .WithCurrentTimestamp();
+
+        return embed;
+    }
+
+    public EmbedBuilder Rolls(int dice, int[] rolls)
+    {
+        var embed = new EmbedBuilder()
+            .WithTitle($"Rolled D{dice}, {rolls.Length} times:\n\nSum: {rolls.Sum()} out of {dice*rolls.Length}")
+            .WithDescription($"Breakdown of rolls:")
+            .WithColor(Color.Orange);
+
+        for(int i = 0; i < rolls.Length; i++) {
+
+            var field = new EmbedFieldBuilder()
+                .WithName($"Roll no. {i+1}")
+                .WithValue(rolls[i])
+                .WithIsInline(false);
+
+            embed.AddField(field);
+        }
+
+        return embed;
+    }
 }

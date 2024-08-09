c711818e5ea75af34f6fff96c16aeb94f53e5da3
smellilac <dimatega@gmail.com>
2024-08-09 16:29:47 +0300

    Added limits for session

--- Nero-source/Commands/Session/SessionCommands.cs
+++ Nero-source/Commands/Session/SessionCommands.cs
@@ -1,30 +1,44 @@
 using Discord;
 using Discord.WebSocket;
+using Nero.Commands.Session;
 
 namespace Nero;
 
 public class SessionCommand
 {
-    public async Task CommandHandler(SocketSlashCommand command) {
-
-        switch (command.Data.Options.First().Name){
+    private readonly SessionManager _sessionManager;
+    private const int maxSessionsPerUser = 10;
+    public SessionCommand()
+        => _sessionManager = new SessionManager(maxSessionsPerUser);
+    public async Task CommandHandler(SocketSlashCommand command)
+    {
+        switch (command.Data.Options.First().Name)
+        {
             case "create":
                 await Create(command);
                 return;
             default:
                 var embeds = new Embeds();
-
                 await command.RespondAsync(embed: embeds.Error("Not implemented yet.").Build());
                 return;
         }
-
     }
 
-    private async Task Create(SocketSlashCommand command) {
+    private async Task Create(SocketSlashCommand command)
+    {
+        var userId = command.User.Id;
+
+        if (!_sessionManager.CanCreateSession(userId))
+        {
+            var embeds = new Embeds();
+            await command.RespondAsync(embed: embeds.Error("You have reached the limit of active sessions.").Build());
+            return;
+        }
 
         var socketChannel = command.Channel as ITextChannel;
 
-        if (socketChannel != null) {
+        if (socketChannel != null)
+        {
             var newSession = await socketChannel.CreateThreadAsync(
                 name: command.Data.Options.First().Options.First().Value.ToString(),
                 autoArchiveDuration: ThreadArchiveDuration.OneWeek,
@@ -34,11 +48,14 @@ public class SessionCommand
 
             string channelMention = $"<#{newSession.Id}>";
 
+            _sessionManager.AddSession(userId, newSession.Id);
+
             await command.RespondAsync($"Session created: {channelMention}");
 
             await newSession.SendMessageAsync($"Session created by: {command.User.Mention}!");
-        } 
-        else {
+        }
+        else
+        {
             var embeds = new Embeds();
             await command.RespondAsync(embed: embeds.Error("Channel Error.").Build());
         }
